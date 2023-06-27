a8d93f236886ba87c8309ce5810cdec71e26e8cc
nhant <14520618@gm.uit.edu.vn>
2023-06-27 21:27:01 +0700

    [ADD]

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const app = require('express')();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
-io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
+  res.json({
+    number: 1,
+    name: 'John',
+    gender: 'male'
   });
 });
+
 
 http.listen(port, () => {
   console.log(`Socket.IO server running at http://localhost:${port}/`);
